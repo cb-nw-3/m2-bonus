5ba23f5f4a5e7c8e0fc3c085804e99b75b9ffe55
AlexandreGagnon-Lalonde <alexandre.gl@hotmail.ca>
2020-05-15 18:00:00 -0400

    bonus art project exercise 1 comments

--- 1-generative-art/Exercise 1/app.js
+++ 1-generative-art/Exercise 1/app.js
@@ -1,14 +1,14 @@
+// select the canvas
 let canvas = document.querySelector('#canvas');
-
+// initialize bunch of variables
 let newRect;
-
 let randomColor;
 let randomLateralPosition;
 let randomVerticalPosition;
 let randomWidth;
 let randomHeight;
 let randomAngle;
-
+// set array of rnadom colors
 let colors = [
   "#A56BFF", 
   "#63DBEB", 
@@ -22,21 +22,31 @@ let colors = [
 ];
 
 function generateArt() {
+  // create 2000 rectangles
   for (let i = 0; i <= 2000; i++) {
+    // random number for color
     randomColor = Math.floor(Math.random() * 8);
+    // random position for top value 0 to 100
     randomVerticalPosition = Math.random() * 100;
+    // random position for left value 0 to 100
     randomLateralPosition = Math.random() * 100;
+    // random width between 20 and 30
     randomWidth = (Math.random() * 10) + 20;
+    // random height between 20 and 30
     randomHeight = (Math.random() * 10) + 20;
+    // random angle for rectangle rotation
     randomAngle = Math.random() * 360;
+    // create the rectangle
     newRect = document.createElement('div');
+    // style the rectangle with styles
     newRect.style.background = colors[randomColor];
     newRect.style.width = `${randomWidth}px`;
     newRect.style.height = `${randomHeight}px`;
     newRect.style.left = `${randomLateralPosition}%`;
     newRect.style.top = `${randomVerticalPosition}%`;
     newRect.style.transform = `rotate(${randomAngle}deg)`
-    newRect.classList.add('rectangle');
+    newRect.style.position = 'absolute';
+    // add rectangle to canvas
     canvas.appendChild(newRect);
   }
 }
