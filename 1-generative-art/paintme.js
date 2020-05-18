cb125dba6f3a7d2d28878d2bab42bad0fafbf517
garorueda99 <59367534+garorueda99@users.noreply.github.com>
2020-05-18 18:13:10 -0400

    art1

---
+++ 1-generative-art/paintme.js
@@ -0,0 +1,47 @@
+const CANVAS = document.createElement("div");
+CANVAS.id = "canvas";
+CANVAS.style.width = "100vw";
+CANVAS.style.height = "100vh";
+document.body.appendChild(CANVAS);
+
+function generateArt() {
+  for (let i = 0; i <= 500; i++) {
+    setTimeout(() => {
+      const BRUSH = document.createElement("canvas");
+      BRUSH.style.width = `${randomSize()}%`;
+      BRUSH.style.height = `${randomSize()}%`;
+      BRUSH.style.backgroundColor = randomBackground();
+      BRUSH.style.position = "absolute";
+      BRUSH.style.top = `${randomPosition()}%`;
+      BRUSH.style.left = `${randomPosition()}%`;
+      BRUSH.style.transform = `rotate(${randomAngle()}deg)`;
+      CANVAS.appendChild(BRUSH);
+    }, 50 * i);
+  }
+}
+
+function randomSize() {
+  return Math.floor(Math.random() * 15) + 1;
+}
+
+function randomColor() {
+  color = [];
+  for (let i = 0; i < 3; i++) {
+    color.push(Math.floor(Math.random() * 256));
+  }
+  return `rgb(${color[0]}, ${color[1]}, ${color[2]})`;
+}
+
+function randomBackground() {
+  return `linear-gradient(${randomColor()}, ${randomColor()})`;
+}
+
+function randomPosition() {
+  return Math.floor(Math.random() * 100);
+}
+
+function randomAngle() {
+  return Math.floor(Math.random() * 360);
+}
+
+generateArt();
